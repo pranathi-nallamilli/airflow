c140cc0587b59e10868ced7cc23d275caeb44234
Niranjan Nareshan <nnareshan@deloitte.com>
2021-08-31 19:58:32 +0530

    initial

--- dags/data_prep_pipeline.py
+++ dags/data_prep_pipeline.py
@@ -22,46 +22,46 @@ default_args = {
 
 with DAG(dag_id='data_prep_pipeline', default_args=default_args, catchup=False) as dag:
 
-    check_azure_connection = PythonOperator(task_id='azure_connection',
-                            python_callable= az.azure_connection,
-                            dag=dag)
+    # check_azure_connection = PythonOperator(task_id='azure_connection',
+    #                         python_callable= az.azure_connection,
+    #                         dag=dag)
 
     save_azure_blob = PythonOperator(task_id='save_azure_blob',
                             python_callable=az.read_azure_blob_file,
                             dag=dag)
 
-    set_iteration_variables = PythonOperator(task_id='set_iteration_variables',
-                            python_callable=gi.set_iteration_variables,
-                            dag=dag)
+    # set_iteration_variables = PythonOperator(task_id='set_iteration_variables',
+    #                         python_callable=gi.set_iteration_variables,
+    #                         dag=dag)
 
-    manifest_file = PythonOperator(task_id='manifest_file',
-                            python_callable=fp.check_file_path,
-                            dag=dag)
+    # manifest_file = PythonOperator(task_id='manifest_file',
+    #                         python_callable=fp.check_file_path,
+    #                         dag=dag)
 
-    validate_file = PythonOperator(task_id='validate_file',
-                        python_callable=dvv.derive_and_validate_file,
-                        dag=dag)
+    # validate_file = PythonOperator(task_id='validate_file',
+    #                     python_callable=dvv.derive_and_validate_file,
+    #                     dag=dag)
 
-    check_snowflake_connection = PythonOperator(task_id='check_snowflake_connection',     
-                            python_callable=snowflake_db.create_snowflake_connection,    
-                            dag=dag )
+    # check_snowflake_connection = PythonOperator(task_id='check_snowflake_connection',     
+    #                         python_callable=snowflake_db.create_snowflake_connection,    
+    #                         dag=dag )
 
-    get_schema = PythonOperator(task_id='get_schema',     
-                            python_callable=gc.initiate_get_schema,    
-                            dag=dag )
+    # get_schema = PythonOperator(task_id='get_schema',     
+    #                         python_callable=gc.initiate_get_schema,    
+    #                         dag=dag )
 
-    read_DS_DT = PythonOperator(
-                            task_id="read_DS_DT_task",
-                            python_callable=read_ds_dt.read_DS_DT_operation,
-                            dag=dag )
+    # read_DS_DT = PythonOperator(
+    #                         task_id="read_DS_DT_task",
+    #                         python_callable=read_ds_dt.read_DS_DT_operation,
+    #                         dag=dag )
 
-    data_catalog_lookup_task = PythonOperator(task_id='data_catalog_lookup',     
-                            python_callable=dc.lookup_data_catalog,    
-                            dag=dag )
+    # data_catalog_lookup_task = PythonOperator(task_id='data_catalog_lookup',     
+    #                         python_callable=dc.lookup_data_catalog,    
+    #                         dag=dag )
 
-    mapping_and_validation = PythonOperator(task_id='mapping_and_validation',     
-                            python_callable=mv.mapping_validation_initiate_operation,    
-                            dag=dag )
+    # mapping_and_validation = PythonOperator(task_id='mapping_and_validation',     
+    #                         python_callable=mv.mapping_validation_initiate_operation,    
+    #                         dag=dag )
 
-    check_azure_connection  >>  save_azure_blob >> set_iteration_variables >> manifest_file >> validate_file >> check_snowflake_connection  >> get_schema >> read_DS_DT >> data_catalog_lookup_task >> mapping_and_validation
+    # check_azure_connection  >>  save_azure_blob #>> set_iteration_variables >> manifest_file >> validate_file >> check_snowflake_connection  >> get_schema >> read_DS_DT >> data_catalog_lookup_task >> mapping_and_validation
     #check_snowflake_connection  >> get_schema >> read_DS_DT
--- dags/utils/azure_operations.py
+++ dags/utils/azure_operations.py
@@ -1,21 +1,44 @@
 from airflow.providers.microsoft.azure.hooks.wasb import WasbHook
 from airflow.models import Variable
 import pandas as pd
-import io
 
-azure = WasbHook(wasb_conn_id='azure_blob')
-container = Variable.get('env_azure_container')
-blob = Variable.get('env_input_blob')
-env_file_path  =Variable.get('env_file_path')
-v_batch_ingestion_blob_output_path = Variable.get('v_batch_ingestion_blob_output_path')
+azure = WasbHook(wasb_conn_id="azure_blob")
+container = Variable.get("env_azure_container")
+blob = Variable.get("env_input_blob")
+env_file_path = Variable.get("env_file_path")
+v_batch_ingestion_blob_output_path = Variable.get("v_batch_ingestion_blob_output_path")
+
+
 def azure_connection():
- return(azure.check_for_blob(container,blob))
+    return azure.check_for_blob(container, blob)
+
+
+def convert_json_to_csv():
+    # TODO: making it into chunks if need be
+    # file = azure.download(container, blob,0)
+    # file._config.max_chunk_get_size = 1000
+    # for chunk in file.chunks():
+    #     print("chunk", chunk)
+    file = azure.read_file(container, blob, max_concurrency=5)
+    dataframe = pd.read_json(file)
+    dataframe.to_csv(env_file_path)
+
+
+def convert_parquet_to_csv():
+    file = azure.read_file(container, blob, max_concurrency=5)
+    dataframe = pd.read_parquet(file)
+    dataframe.to_csv(env_file_path)
+
 
 def read_azure_blob_file():
-    file = azure.read_file(container,blob)
-    df = pd.read_csv(io.StringIO(file)  , sep=",")
-    print(df)
-    df.to_csv(env_file_path, index=False)
+    file_ext = blob.split(".")[-1].upper()
+    if file_ext == "CSV":
+        azure.get_file(env_file_path, container, blob)
+    elif file_ext == "JSON":
+        convert_json_to_csv()
+    elif file_ext == "PARQUET":
+        convert_parquet_to_csv()
+
 
 def write_to_azure_blob():
-    azure.load_file(env_file_path,container,v_batch_ingestion_blob_output_path)
+    azure.load_file(env_file_path, container, v_batch_ingestion_blob_output_path)
